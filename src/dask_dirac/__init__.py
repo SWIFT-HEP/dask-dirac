64659e45ee428e4a7872d3131cada7d027688c6e
Luke Kreczko <lkreczko@googlemail.com>
2022-04-28 16:20:01 +0100

    initial commit

---
+++ src/dask_dirac/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) 2022 Luke Kreczko. All rights reserved.
+
+dask-dirac: DIRAC Executor for Dask
+"""
+
+
+from __future__ import annotations
+
+__version__ = "0.1.0"
+
+__all__ = ("__version__",)
