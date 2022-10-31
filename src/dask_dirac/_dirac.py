42b1296bca109ef883b1ef2656b02de9b597abb7
Luke Kreczko <lkreczko@googlemail.com>
2022-10-31 16:26:22 +0000

    fix: mypy, flake8 and docs

--- src/dask_dirac/_dirac.py
+++ src/dask_dirac/_dirac.py
@@ -5,30 +5,39 @@ from typing import Any
 import requests
 
 
-def _query(url: str, params: dict[str, str], capath:str, user_proxy:str) -> Any:
+def _query(url: str, params: dict[str, str], capath: str, user_proxy: str) -> Any:
     if not capath:
         capath = "/etc/grid-security/certificates"
     if not user_proxy:
-        user_proxy = "/tmp/x509up_u1000"  # TODO: default path should be /tmp/x509up_u<user id>
+        user_proxy = (
+            "/tmp/x509up_u1000"  # TODO: default path should be /tmp/x509up_u<user id>
+        )
     with requests.post(url, data=params, cert=user_proxy, verify=capath) as r:
         return r.json()
 
 
-def submit_job(server_url: str, jdl: str, capath:str, user_proxy:str) -> Any:
+def submit_job(server_url: str, jdl: str, capath: str, user_proxy: str) -> Any:
     endpoint = "WorkloadManagement/JobManager"
     url = f"{server_url}/{endpoint}"
     params = {"method": "submitJob", "jobDesc": jdl}
     return _query(url, params, capath, user_proxy)
 
 
-def get_jobs(server_url: str, capath:str, user_proxy:str) -> Any:
+def get_jobs(server_url: str, capath: str, user_proxy: str) -> Any:
     endpoint = "WorkloadManagement/JobMonitoring"
     url = f"{server_url}/{endpoint}"
     params = {"method": "getJobs"}
     return _query(url, params, capath, user_proxy)
 
 
-def whoami(server_url: str, capath:str, user_proxy:str) -> Any:
+def get_max_parametric_jobs(server_url: str, capath: str, user_proxy: str) -> Any:
+    endpoint = "WorkloadManagement/JobManager"
+    url = f"{server_url}/{endpoint}"
+    params = {"method": "getMaxParametricJobs"}
+    return _query(url, params, capath, user_proxy)
+
+
+def whoami(server_url: str, capath: str, user_proxy: str) -> Any:
     endpoint = "DataManagement/FileCatalog"
     url = f"{server_url}/{endpoint}"
     params = {"method": "whoami"}
--- src/dask_dirac/cli.py
+++ src/dask_dirac/cli.py
@@ -1,47 +1,80 @@
-import typer
+from __future__ import annotations
+
 from typing import Any
 
-from . import _dirac
-from . import __version__
+import typer
+
+from . import __version__, _dirac
 
 app = typer.Typer()
 
+
 @app.command()
 def whoami(
-    server_url: str, # "https://dirac.gridpp.ac.uk:8443"
-    capath: str = typer.Option(default="/etc/grid-security/certificates", help="path to CA certificate directory"),
-    user_proxy: str = typer.Option(default="/tmp/x509up_u1000", help="path to user proxy"),
-    ) -> None:
+    server_url: str,  # "https://dirac.gridpp.ac.uk:8443"
+    capath: str = typer.Option(
+        default="/etc/grid-security/certificates",
+        help="path to CA certificate directory",
+    ),
+    user_proxy: str = typer.Option(
+        default="/tmp/x509up_u1000", help="path to user proxy"
+    ),
+) -> None:
     """Print the DN of the current user as seen by DIRAC server"""
     result = _dirac.whoami(server_url, capath, user_proxy)
+    typer.echo(result)
+
 
 @app.command()
 def submit(
-    server_url: str, # "https://dirac.gridpp.ac.uk:8443"
-    jdl_file: str, # "job.jdl"
-    capath: str = typer.Option(default="/etc/grid-security/certificates", help="path to CA certificate directory"),
-    user_proxy: str = typer.Option(default="/tmp/x509up_u1000", help="path to user proxy"),
-    ) -> None:
+    server_url: str,  # "https://dirac.gridpp.ac.uk:8443"
+    jdl_file: str,  # "job.jdl"
+    capath: str = typer.Option(
+        default="/etc/grid-security/certificates",
+        help="path to CA certificate directory",
+    ),
+    user_proxy: str = typer.Option(
+        default="/tmp/x509up_u1000", help="path to user proxy"
+    ),
+) -> None:
     """Submit a job to DIRAC server"""
+    with open(jdl_file) as f:
+        jdl = f.read()
     result = _dirac.submit_job(server_url, jdl, capath, user_proxy)
+    typer.echo(result)
+
 
 @app.command()
 def status(
-    server_url: str, # "https://dirac.gridpp.ac.uk:8443"
-    capath: str = typer.Option(default="/etc/grid-security/certificates", help="path to CA certificate directory"),
-    user_proxy: str = typer.Option(default="/tmp/x509up_u1000", help="path to user proxy"),
-    ) -> None:
+    server_url: str,  # "https://dirac.gridpp.ac.uk:8443"
+    capath: str = typer.Option(
+        default="/etc/grid-security/certificates",
+        help="path to CA certificate directory",
+    ),
+    user_proxy: str = typer.Option(
+        default="/tmp/x509up_u1000", help="path to user proxy"
+    ),
+) -> None:
     """Print the list of jobs"""
     result = _dirac.get_jobs(server_url, capath, user_proxy)
+    typer.echo(result)
+
 
 @app.command()
 def get_max_parametric_jobs(
-    server_url: str, # "https://dirac.gridpp.ac.uk:8443"
-    capath: str = typer.Option(default="/etc/grid-security/certificates", help="path to CA certificate directory"),
-    user_proxy: str = typer.Option(default="/tmp/x509up_u1000", help="path to user proxy"),
-    ) -> None:
+    server_url: str,  # "https://dirac.gridpp.ac.uk:8443"
+    capath: str = typer.Option(
+        default="/etc/grid-security/certificates",
+        help="path to CA certificate directory",
+    ),
+    user_proxy: str = typer.Option(
+        default="/tmp/x509up_u1000", help="path to user proxy"
+    ),
+) -> None:
     """Print the maximum number of parametric jobs"""
     result = _dirac.get_max_parametric_jobs(server_url, capath, user_proxy)
+    typer.echo(result)
+
 
 @app.command()
 def version() -> None:
