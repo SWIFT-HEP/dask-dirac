d361c84c6faf6473cef7f716d1f5059cd9f13979
Luke Kreczko <lkreczko@googlemail.com>
2022-10-31 14:47:13 +0000

    refactor: server --> server_url

--- src/dask_dirac/_dirac.py
+++ src/dask_dirac/_dirac.py
@@ -14,19 +14,22 @@ def _query(url: str, params: dict[str, str], capath:str, user_proxy:str) -> Any:
         return r.json()
 
 
-def submit_job(server: str, jdl: str, capath:str, user_proxy:str) -> Any:
-    url = f"https://{server}:8443/WorkloadManagement/JobManager"
+def submit_job(server_url: str, jdl: str, capath:str, user_proxy:str) -> Any:
+    endpoint = "WorkloadManagement/JobManager"
+    url = f"{server_url}/{endpoint}"
     params = {"method": "submitJob", "jobDesc": jdl}
     return _query(url, params, capath, user_proxy)
 
 
-def get_jobs(server: str, capath:str, user_proxy:str) -> Any:
-    url = f"https://{server}:8443/WorkloadManagement/JobMonitoring"
+def get_jobs(server_url: str, capath:str, user_proxy:str) -> Any:
+    endpoint = "WorkloadManagement/JobMonitoring"
+    url = f"{server_url}/{endpoint}"
     params = {"method": "getJobs"}
     return _query(url, params, capath, user_proxy)
 
 
-def whoami(server: str, capath:str, user_proxy:str) -> Any:
-    url = f"https://{server}:8443/DataManagement/FileCatalog"
+def whoami(server_url: str, capath:str, user_proxy:str) -> Any:
+    endpoint = "DataManagement/FileCatalog"
+    url = f"{server_url}/{endpoint}"
     params = {"method": "whoami"}
     return _query(url, params, capath, user_proxy)
