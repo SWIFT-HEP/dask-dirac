973b693199a268e4252da914a002793d0a6f41b8
Luke Kreczko <lkreczko@googlemail.com>
2022-10-31 14:40:09 +0000

    refactor: capath and user_proxy are now parameters

--- src/dask_dirac/_dirac.py
+++ src/dask_dirac/_dirac.py
@@ -5,26 +5,28 @@ from typing import Any
 import requests
 
 
-def _query(url: str, params: dict[str, str]) -> Any:
-    capath = "/etc/grid-security/certificates"  # TODO: should be a parameter
-    cert = "/tmp/x509up_u1000"  # TODO: default path should be /tmp/x509up_u<user id>
-    with requests.post(url, data=params, cert=cert, verify=capath) as r:
+def _query(url: str, params: dict[str, str], capath:str, user_proxy:str) -> Any:
+    if not capath:
+        capath = "/etc/grid-security/certificates"
+    if not user_proxy:
+        user_proxy = "/tmp/x509up_u1000"  # TODO: default path should be /tmp/x509up_u<user id>
+    with requests.post(url, data=params, cert=user_proxy, verify=capath) as r:
         return r.json()
 
 
-def submit_job(server: str, jdl: str) -> Any:
+def submit_job(server: str, jdl: str, capath:str, user_proxy:str) -> Any:
     url = f"https://{server}:8443/WorkloadManagement/JobManager"
     params = {"method": "submitJob", "jobDesc": jdl}
-    return _query(url, params)
+    return _query(url, params, capath, user_proxy)
 
 
-def get_jobs(server: str) -> Any:
+def get_jobs(server: str, capath:str, user_proxy:str) -> Any:
     url = f"https://{server}:8443/WorkloadManagement/JobMonitoring"
     params = {"method": "getJobs"}
-    return _query(url, params)
+    return _query(url, params, capath, user_proxy)
 
 
-def whoami(server: str) -> Any:
+def whoami(server: str, capath:str, user_proxy:str) -> Any:
     url = f"https://{server}:8443/DataManagement/FileCatalog"
     params = {"method": "whoami"}
-    return _query(url, params)
+    return _query(url, params, capath, user_proxy)
