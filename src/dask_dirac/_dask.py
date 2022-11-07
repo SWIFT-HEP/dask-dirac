4ba201d772be2a22fdb2f751fa64c2fe30e51893
Luke Kreczko <lkreczko@googlemail.com>
2022-11-07 10:21:01 +0000

    feat (draft): add _dask module

---
+++ src/dask_dirac/_dask.py
@@ -0,0 +1,43 @@
+"""Definitions for DaskCluster"""
+
+# from dask.distributed import
+from __future__ import annotations
+
+from typing import Any
+
+from dask_jobqueue.core import Job, JobQueueCluster, cluster_parameters, job_parameters
+
+
+class DiracJob(Job):
+    def __init__(
+        self,
+        scheduler: Any = None,
+        name: str | None = None,
+        config_name: str | None = None,
+        **base_class_kwargs: dict[str, Any],
+    ) -> None:
+        super().__init__(
+            scheduler=scheduler, name=name, config_name=config_name, **base_class_kwargs
+        )
+
+
+class DiracCluster(JobQueueCluster):
+    __doc__ = f""" Launch Dask on a cluster via Dirac
+
+    Parameters
+    ----------
+    server_url: str
+        URL to the DIRAC instance
+    {job_parameters}
+    {cluster_parameters}
+
+    Examples
+    --------
+    >>> from dask_dirac import DiracCluster
+    >>> cluster = DiracCluster(server_url="https://.....:8443", user_proxy="/tmp/X509_proxy")
+    >>> cluster.scale(jobs=10)
+
+    >>> from dask.distributed import Client
+    >>> client = Client(cluster)
+    """
+    job_cls = DiracJob
