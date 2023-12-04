e2f848659b30e8aa8573153cfeccffa4cf5c9e86
kreczko <lkreczko@googlemail.com>
2023-12-04 13:36:18 +0000

    fix: add docs to template module

--- src/dask_dirac/templates/__init__.py
+++ src/dask_dirac/templates/__init__.py
@@ -1,18 +1,19 @@
+"""Dask Dirac Template Module"""
 from __future__ import annotations
+
+from functools import cache
 
 from jinja2 import Environment, PackageLoader, Template, select_autoescape
 
-env = None
 
-
+@cache
 def get_jinja_env() -> Environment:
-    global env
-    if env is None:
-        env = Environment(
-            loader=PackageLoader("dask_dirac"), autoescape=select_autoescape()
-        )
-    return env
+    """Get the project-specific jinja environment"""
+    return Environment(
+        loader=PackageLoader("dask_dirac"), autoescape=select_autoescape()
+    )
 
 
 def get_template(template_name: str) -> Template:
+    """Attempts to retrieve template named {template_name}"""
     return get_jinja_env().get_template(template_name)
