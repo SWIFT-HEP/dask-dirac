8bf1562448e3efc3a0ddf0751e8ec416ba64949e
seriksen <sam.eriksen@bristol.ac.uk>
2025-02-10 09:40:14 +0000

    fix: give access to DiracClient

--- src/dask_dirac/__init__.py
+++ src/dask_dirac/__init__.py
@@ -6,8 +6,8 @@ dask-dirac: DIRAC Executor for Dask
 
 from __future__ import annotations
 
-from ._dask import DiracCluster, DiracClient
+from ._dask import DiracClient, DiracCluster
 from ._version import get_versions
 
 __version__ = get_versions()["version"]
-__all__ = ("__version__", "DiracCluster")
+__all__ = ("__version__", "DiracCluster", "DiracClient")
